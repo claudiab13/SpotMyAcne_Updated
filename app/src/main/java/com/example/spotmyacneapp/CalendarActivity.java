e17ca33a4e127c19568548122e4f0efeb367fac1
claudia <claudia.burcea@student.upt.ro>
2024-01-13 23:48:30 +0200

    Import Camera

--- app/src/main/java/com/example/spotmyacneapp/CalendarActivity.java
+++ app/src/main/java/com/example/spotmyacneapp/CalendarActivity.java
@@ -1,34 +1,96 @@
 package com.example.spotmyacneapp;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.Calendar;
+
 public class CalendarActivity extends AppCompatActivity {
+    private static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    CalendarView calendarView;
+    Calendar calendar;
+    ImageView photo;
+    ImageButton take_photo;
+    ImageButton back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        ImageButton back = findViewById(R.id.backCalendar);
 
+        back = findViewById(R.id.backCalendar);
         back.setOnClickListener(view -> {
             Intent intent = new Intent(CalendarActivity.this, HomeActivity.class);
             startActivity(intent);
         });
 
-       /*CalendarView calendarView = findViewById(R.id.calendarView);
-
-        calendarView.setOnFocusChangeListener(new OnDateSelectedListener() {
+        calendarView = findViewById(R.id.calendarView);
+        calendar = Calendar.getInstance();
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                // Aici se va deschide noul ecran când utilizatorul selectează o zi în calendar
-                openNewScreen(date);
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                month = month + 1;
+                Toast.makeText(CalendarActivity.this, day + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
             }
         });
-*/
 
+        photo = findViewById(R.id.photo);
+        take_photo = findViewById(R.id.take_photo);
+        take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askCameraPermissions();
+            }
+        });
+    }
 
+    private void askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+            openCamera();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERM_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(this, "Camera ermission is required to use camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCamera() {
+        //Toast.makeText(this, "Camera Open Request", Toast.LENGTH_SHORT).show();
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @NonNull Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            photo.setImageBitmap(image);
+        }
     }
 }
+
---
+++ app/src/main/java/com/example/spotmyacneapp/DailyRoutineActivity.java
@@ -0,0 +1,4 @@
+package com.example.spotmyacneapp;
+
+public class DailyRoutineActivity {
+}
